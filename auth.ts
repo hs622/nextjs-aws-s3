0fa56e2a16d80a7c924fcec6dd46f96f76f8d998
Hussain Ali <hussainalee@icloud.com>
2025-11-15 00:58:36 +0500

    Update session token handling in auth middleware and configuration

--- auth.ts
+++ auth.ts
@@ -2,6 +2,16 @@ import NextAuth from "next-auth";
 import GoogleProvider from "./app/api/auth/providers/google-provider";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  cookies: {
+    sessionToken: {
+      name: "session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/"
+      }
+    }
+  },
   providers: [GoogleProvider],
   pages: {
     signIn: "/login",
